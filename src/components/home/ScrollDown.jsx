044e808739a2103cd9819ac1b79726b02a8e4d0b
Shashank <shashank_official@hotmail.com>
2024-01-24 00:33:15 +0530

    structure done

---
+++ src/components/home/ScrollDown.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ScrollDown = () => {
+  return (
+    <div className="scroll__down">
+      <a href="" className="mouse__wrapper">
+        <span className="home__scroll-name">
+          Scroll Down
+          <span className="mouse">
+            <span className="wheel"></span>
+          </span>
+        </span>
+      </a>
+    </div>
+  );
+};
+
+export default ScrollDown;
